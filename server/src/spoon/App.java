acf3acf7ae28256ab8a37a4b95d6ac02e94c4263
HediMokhtar <hedmokhtar@gmail.com>
2016-10-10 20:41:53 +0200

    resolution of the BuildingModelException

--- server/src/spoon/App.java
+++ server/src/spoon/App.java
@@ -1,30 +1,43 @@
 package spoon;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
-	
+
     public static void main(String[] args) throws IOException {
-    	
+
+
+    	long debut = System.currentTimeMillis();
         final Launcher launcher = new Launcher();
         final String repositoryPath = Pwd.getRepositoryPath();
-        final String outputDirectory = Pwd.getOutputPath();
-        
-        launcher.addInputResource(repositoryPath);        
-        launcher.setSourceOutputDirectory(outputDirectory);
-        
-        launcher.addProcessor(new ClassProcessor());
-        launcher.run();
-    
-        
+
+        final List<String> arguments = new LinkedList<String>();
+        arguments.add("-i");
+        arguments.add(repositoryPath);
+        arguments.add("-p");
+        arguments.add(ClassProcessor.class.getName());
+        arguments.add("-x");
+
+        launcher.run(arguments.toArray(new String[arguments.size()]));
+
+//        launcher.addInputResource(repositoryPath);
+//        launcher.setSourceOutputDirectory(outputDirectory);
+//
+//        launcher.addProcessor(new ClassProcessor());
+//        launcher.run();
+
+
         System.out.println("Before analyse : ");
         ClassRanking rank = ClassProcessor.getRank();
         System.out.println(rank.toString());
-        
+
         System.out.println("After analyse : ");
         ClassProcessor.analyse();
         System.out.println(rank.toString());
-	
+
+        System.out.println(System.currentTimeMillis() - debut + "ms");
     }
-    
+
 }
